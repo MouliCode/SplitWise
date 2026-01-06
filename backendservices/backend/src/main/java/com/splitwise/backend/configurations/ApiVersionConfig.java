7a6b5b85f04228fcda6ce7edb64dd501396f05d8
Lee <bcmouli11@gmail.com>
2026-01-06 23:04:51 +0530

    added configuration files

---
+++ backendservices/backend/src/main/java/com/splitwise/backend/configurations/ApiVersionConfig.java
@@ -0,0 +1,17 @@
+package com.splitwise.backend.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiVersionConfig  implements WebMvcConfigurer {
+	
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer){
+		configurer.addPathPrefix (
+				"/api/v1",
+				clazz -> clazz.isAnnotationPresent (org.springframework.web.bind.annotation.RestController.class)
+		);
+	}
+}
