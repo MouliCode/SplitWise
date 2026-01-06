31672e0509a9d86c374bd899dc650db94a2a66ef
Lee <bcmouli11@gmail.com>
2026-01-06 23:04:51 +0530

    added configuration files

--- backendservices/backend/src/main/java/com/splitwise/backend/common/exception/StandardResponseCode.java
+++ backendservices/backend/src/main/java/com/splitwise/backend/common/exception/StandardResponseCode.java
@@ -1,28 +1,32 @@
 package com.splitwise.backend.common.exception;
 
 public enum StandardResponseCode {
-
-     SUCCESS("success"),
-     VALIDATION_ERROR("invalid request data"),
-     INTERNAL_ERROR("something went wrong"),
-     FAILURE("failure");
-
-     private final String message;
-
-     StandardResponseCode(){
-         this.message = name().toLowerCase();
-     }
-
-     StandardResponseCode(String message){
-        this.message = message;
-     }
-
-     @Override
-    public String toString(){
-         return message;
-     }
-
-    public String getMessage() {
-         return message;
-    }
+	
+	SUCCESS ("success"),
+	VALIDATION_ERROR ("invalid request data"),
+	INTERNAL_ERROR ("something went wrong"),
+	FAILURE ("failure"),
+	EMAIL_OR_PHONE_ERROR ("email or phone number is required"),
+	EMAIL_EXISTS ("email already exists"),
+	PHONE_NUMBER_EXISTS ("phone number already exists"),
+	PASSWORD_ERROR("invalid password");
+	
+	private final String message;
+	
+	StandardResponseCode () {
+		this.message = name ().toLowerCase ();
+	}
+	
+	StandardResponseCode (String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString () {
+		return message;
+	}
+	
+	public String getMessage () {
+		return message;
+	}
 }
